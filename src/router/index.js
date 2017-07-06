257fb83a6a183de40820905fa2a138a5dc7b3ce5
adunham95 <adunham95@gmail.com>
2017-07-06 13:21:15 -0400

    Added mobile viewport and reverted to vuetify

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import VueMaterial from 'vue-material'
+import Vuetify from 'vuetify';
 import VueResource from 'vue-resource';
 import Router from 'vue-router';
 import 'vue-awesome/icons';
@@ -9,17 +9,8 @@ import Home from '@/views/home';
 import Rockets from '@/views/rockets';
 
 Vue.use(Router);
-Vue.use(VueMaterial);
+Vue.use(Vuetify);
 Vue.use(VueResource);
-
-Vue.material.registerTheme('default', {
-    primary: 'blue',
-    accent: 'red',
-    warn: 'red',
-    background: 'grey'
-});
-
-Vue.material.setCurrentTheme('default');
 
 export default new Router({
     routes: [
