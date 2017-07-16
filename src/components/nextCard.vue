21153037714840f94bbe6e6c97475369598293a1
adunham95 <Apd@21295>
2017-07-15 23:30:52 -0400

    created statusCard to display the launch status across the app

--- src/components/nextCard.vue
+++ src/components/nextCard.vue
@@ -5,7 +5,6 @@
                 <v-card-title primary-title>
                     <div class="headline">
                         {{ launch.name }}
-                        <!--<v-chip :class="[status.css]">{{ status.title }} </v-chip>-->
                         <status-chip :statusID="launch.status"></status-chip>
                     </div>
                 </v-card-title>
@@ -19,33 +18,15 @@
 </template>
 
 <script>
-    import StatusChip from "./statucChip";
+    import StatusChip from "./statusChip";
     export default {
         components: {StatusChip},
         name: 'nextCard',
         props: ['launch'],
-        mounted() {
-          if(this.launch.status === 1) {
-//              console.log("Green");
-              this.status.id = 1;
-              this.status.title = 'Green';
-              this.status.css = 'green white--text'
-          }
-          else if(this.launch.status === 2) {
-//              console.log("Red");
-              this.status.id = 2;
-              this.status.title = 'Red';
-              this.status.css = 'red white--text'
-          }
-          else  {
-              console.log("Null")
-          }
-        },
         data() {
             return {
                 title: 'nextCard',
                 dialog: false,
-                status: {id: '', title: '', css: ''},
             };
         },
     };
---
+++ src/components/statusChip.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="statusChip">
+        <v-chip :class="[status.css]">{{ status.title }}</v-chip>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'statusChip',
+        props:['statusID'],
+        mounted() {
+            console.log(this.statusID);
+            if(this.statusID === 1) {
+//              console.log("Green");
+                this.status.id = 1;
+                this.status.title = 'Green';
+                this.status.css = 'green white--text'
+            }
+            else if(this.statusID === 2) {
+//              console.log("Red");
+                this.status.id = 2;
+                this.status.title = 'Red';
+                this.status.css = 'red white--text'
+            }
+            else  {
+                console.log("Null")
+            }
+        },
+        data() {
+            return {
+                title: 'statusChip',
+                status: {id: '', title: '', css: ''},
+            };
+        },
+    };
+</script>
+
+<style>
+
+</style>
